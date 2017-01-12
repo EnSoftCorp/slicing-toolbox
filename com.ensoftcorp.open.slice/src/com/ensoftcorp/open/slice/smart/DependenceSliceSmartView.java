8c40172637ad082e81b8f1293bd7351065e7c63e
Ben Holland <benjholla@gmail.com>
2017-01-11 21:07:47 -0600

    added complete dependence graphs for selections of methods

--- com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/smart/DependenceSliceSmartView.java
+++ com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/smart/DependenceSliceSmartView.java
@@ -7,6 +7,7 @@ import com.ensoftcorp.atlas.core.db.graph.Node;
 import com.ensoftcorp.atlas.core.db.set.AtlasHashSet;
 import com.ensoftcorp.atlas.core.db.set.AtlasSet;
 import com.ensoftcorp.atlas.core.highlight.Highlighter;
+import com.ensoftcorp.atlas.core.log.Log;
 import com.ensoftcorp.atlas.core.markup.MarkupFromH;
 import com.ensoftcorp.atlas.core.query.Q;
 import com.ensoftcorp.atlas.core.script.Common;
@@ -17,6 +18,7 @@ import com.ensoftcorp.atlas.ui.scripts.selections.FilteringAtlasSmartViewScript;
 import com.ensoftcorp.atlas.ui.scripts.selections.IResizableScript;
 import com.ensoftcorp.atlas.ui.selection.event.IAtlasSelectionEvent;
 import com.ensoftcorp.open.commons.analysis.StandardQueries;
+import com.ensoftcorp.open.commons.utilities.DisplayUtils;
 import com.ensoftcorp.open.commons.utilities.FormattedSourceCorrespondence;
 import com.ensoftcorp.open.slice.analysis.DataDependenceGraph;
 import com.ensoftcorp.open.slice.analysis.DependenceGraph;
@@ -26,7 +28,7 @@ public abstract class DependenceSliceSmartView extends FilteringAtlasSmartViewSc
 
 	@Override
 	protected String[] getSupportedNodeTags() {
-		return new String[] { XCSG.ControlFlow_Node, XCSG.DataFlow_Node };
+		return new String[] { XCSG.ControlFlow_Node, XCSG.DataFlow_Node, XCSG.Function };
 	}
 	
 	@Override
@@ -68,9 +70,10 @@ public abstract class DependenceSliceSmartView extends FilteringAtlasSmartViewSc
 	public FrontierStyledResult evaluate(IAtlasSelectionEvent event, int reverse, int forward) {
 		
 		Q filteredSelection = filter(event.getSelection());
-		AtlasSet<Node> criteria = DependenceGraph.getStatements(filteredSelection.eval().nodes());
+		Q functions = filteredSelection.nodesTaggedWithAny(XCSG.Function);
+		AtlasSet<Node> criteria = DependenceGraph.getStatements(filteredSelection.difference(functions).eval().nodes());
 		
-		if(criteria.isEmpty()){
+		if(criteria.isEmpty() && functions.eval().nodes().isEmpty()){
 			return null; // no applicable selection, do nothing
 		}
 		
@@ -81,22 +84,30 @@ public abstract class DependenceSliceSmartView extends FilteringAtlasSmartViewSc
 //			Log.warning("Error summarizing slicing criteria", e);
 //		}
 		
+		// for each selected function, just show the whole dependence graph
+		Q completeDependenceGraphs = Common.empty();
+		for(Node function : functions.eval().nodes()){
+			DependenceGraph dg = getDependenceGraph(function);
+			completeDependenceGraphs = completeDependenceGraphs.union(dg.getGraph());
+		}
+		
+		// create the slice for each function's statement 
 		Q completeResult = Common.empty();
-		for(Node method : StandardQueries.getContainingFunctions(Common.toQ(criteria)).eval().nodes()){
-			Q relevantCriteria = Common.toQ(method).contained().intersection(Common.toQ(criteria));
-			DependenceGraph dg = getDependenceGraph(method);
+		for(Node function : StandardQueries.getContainingFunctions(Common.toQ(criteria)).eval().nodes()){
+			Q relevantCriteria = Common.toQ(function).contained().intersection(Common.toQ(criteria));
+			DependenceGraph dg = getDependenceGraph(function);
 			completeResult = Common.toQ(completeResult.union(dg.getSlice(SliceDirection.BI_DIRECTIONAL, relevantCriteria.eval().nodes())).eval());
 		}
 		
 		// make sure the selected criteria is highlighted 
 		// (since it may have been computed and not be what the user actually selected)
 		Highlighter h = new Highlighter();
-		h.highlight(Common.toQ(criteria), Color.CYAN);
+		h.highlight(Common.toQ(criteria).union(functions), Color.CYAN);
 		
 		// compute what to show for current steps
 		Q f = Common.toQ(criteria).forwardStepOn(completeResult, forward);
 		Q r = Common.toQ(criteria).reverseStepOn(completeResult, reverse);
-		Q result = f.union(r).union(Common.toQ(criteria));
+		Q result = f.union(r).union(Common.toQ(criteria), completeDependenceGraphs);
 		
 		// compute what is on the frontier
 		Q frontierForward = Common.toQ(criteria).forwardStepOn(completeResult, forward+1);
