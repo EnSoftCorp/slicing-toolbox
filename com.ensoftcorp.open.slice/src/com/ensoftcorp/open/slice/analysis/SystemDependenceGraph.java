e4e8f6533707c88ca362a7765c183baec9ef98d1
Ben Holland <benjholla@gmail.com>
2017-09-08 12:17:02 -0500

    bumped version number, added place holder for SDGs

---
+++ com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/analysis/SystemDependenceGraph.java
@@ -0,0 +1,18 @@
+package com.ensoftcorp.open.slice.analysis;
+
+import com.ensoftcorp.atlas.core.db.graph.Graph;
+import com.ensoftcorp.atlas.core.query.Q;
+import com.ensoftcorp.atlas.core.script.Common;
+
+public class SystemDependenceGraph extends DependenceGraph {
+	
+	public SystemDependenceGraph(){
+		throw new RuntimeException("Not Implemented!");
+	}
+	
+	@Override
+	public Q getGraph() {
+		throw new RuntimeException("Not Implemented!");
+	}
+
+}
