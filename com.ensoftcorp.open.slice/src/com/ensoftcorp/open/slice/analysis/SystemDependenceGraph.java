91b42aeff3e4d22e21141d9723f5c60455620199
benjholla <benjholla@gmail.com>
2017-09-18 13:45:49 -0500

    wip SDG

--- com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/analysis/SystemDependenceGraph.java
+++ com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/analysis/SystemDependenceGraph.java
@@ -1,18 +1,34 @@
 package com.ensoftcorp.open.slice.analysis;
 
 import com.ensoftcorp.atlas.core.db.graph.Graph;
+import com.ensoftcorp.atlas.core.db.graph.Node;
 import com.ensoftcorp.atlas.core.query.Q;
 import com.ensoftcorp.atlas.core.script.Common;
+import com.ensoftcorp.atlas.core.xcsg.XCSG;
+import com.ensoftcorp.open.commons.analysis.CommonQueries;
 
 public class SystemDependenceGraph extends DependenceGraph {
 	
+	private static Graph sdg = null;
+	
 	public SystemDependenceGraph(){
-		throw new RuntimeException("Not Implemented!");
+		if(sdg == null){
+			sdg = Common.empty().eval();
+			for(Node function : Common.universe().nodes(XCSG.Function).eval().nodes()){
+				Graph dfg = CommonQueries.dfg(Common.toQ(function)).eval();
+				Graph cfg = CommonQueries.cfg(Common.toQ(function)).eval();
+				Graph pdg = new ProgramDependenceGraph(cfg, dfg).getGraph().eval();
+				sdg.nodes().addAll(pdg.nodes());
+				sdg.edges().addAll(pdg.edges());
+			}
+		}
+		// todo: add data dependence from parameters and returns
+		// todo: add call graph edges
 	}
 	
 	@Override
 	public Q getGraph() {
-		throw new RuntimeException("Not Implemented!");
+		return Common.toQ(sdg);
 	}
 
 }
