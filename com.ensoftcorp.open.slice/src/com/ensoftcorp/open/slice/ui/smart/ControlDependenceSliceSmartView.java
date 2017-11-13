bc3b93729b07ed9855bf23008857fb1b731aa550
benjholla <benjholla@gmail.com>
2017-11-13 11:33:02 -0600

    adding smart views back until code painters are ready for prime time

---
+++ com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/ui/smart/ControlDependenceSliceSmartView.java
@@ -0,0 +1,18 @@
+package com.ensoftcorp.open.slice.ui.smart;
+
+import com.ensoftcorp.atlas.core.db.graph.Node;
+import com.ensoftcorp.open.slice.analysis.DependenceGraph;
+
+public class ControlDependenceSliceSmartView extends DependenceSliceSmartView {
+
+	@Override
+	public String getTitle() {
+		return "Control Dependence Slice (CDG)";
+	}
+	
+	@Override
+	protected DependenceGraph getDependenceGraph(Node function) {
+		return DependenceGraph.Factory.buildCDG(function);
+	}
+
+}
---
+++ com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/ui/smart/ProgramDependenceSliceSmartView.java
@@ -0,0 +1,18 @@
+package com.ensoftcorp.open.slice.ui.smart;
+
+import com.ensoftcorp.atlas.core.db.graph.Node;
+import com.ensoftcorp.open.slice.analysis.DependenceGraph;
+
+public class ProgramDependenceSliceSmartView extends DependenceSliceSmartView {
+
+	@Override
+	public String getTitle() {
+		return "Program Dependence Slice (PDG)";
+	}
+	
+	@Override
+	protected DependenceGraph getDependenceGraph(Node function) {
+		return DependenceGraph.Factory.buildPDG(function);
+	}
+	
+}
