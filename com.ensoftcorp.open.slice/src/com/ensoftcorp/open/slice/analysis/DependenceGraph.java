6e47142a813845bb492f8d6d3c4c2e6c00a87f26
Payas Awadhutkar <payasra@gmail.com>
2020-05-12 15:06:10 -0500

    Getting rid of CDataDependenceGraph for now

--- com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/analysis/DependenceGraph.java
+++ com.ensoftcorp.open.slice/src/com/ensoftcorp/open/slice/analysis/DependenceGraph.java
@@ -88,7 +88,7 @@ public abstract class DependenceGraph {
 			
 			if(function.taggedWith(XCSG.Language.C)) {
 				Q dfg = Common.toQ(function).contained().nodes(XCSG.DataFlow_Node);
-				DependenceGraph ddg = new CDataDependenceGraph(dfg.eval());
+				DependenceGraph ddg = new DataDependenceGraph(dfg.eval());
 				return ddg;
 			}
 			else {
